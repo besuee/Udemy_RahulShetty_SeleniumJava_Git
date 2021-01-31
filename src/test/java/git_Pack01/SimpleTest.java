9a9f60a6e6e0f95d3f6745dd483c2426804b70ff
Kausik Dutta <kd.datasc@gmail.com>
2021-01-31 11:10:09 +0530

    First commit to add all the initial ready Project files

---
+++ src/test/java/git_Pack01/SimpleTest.java
@@ -0,0 +1,12 @@
+package git_Pack01;
+
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+	
+	@Test
+	public void printSimpleTest() {
+		System.out.println("First Basic Simple Test for Git");
+	}
+
+}
