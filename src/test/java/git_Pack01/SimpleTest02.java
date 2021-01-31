54b7a2db129bdae60604c6152122dc8c96bac026
Kausik Dutta <kd.datasc@gmail.com>
2021-01-31 15:04:32 +0530

    Added new class & method under Master Branch

---
+++ src/test/java/git_Pack01/SimpleTest02.java
@@ -0,0 +1,12 @@
+package git_Pack01;
+
+import org.testng.annotations.Test;
+
+public class SimpleTest02 {
+
+	@Test
+	public void printSimpleTest02() {
+		
+		System.out.println("Added new Class & Method under Master Branch");
+	}
+}
